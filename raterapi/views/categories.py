f8602612c91bb1b708b3e73662a2b4594f0fff38
Nate Z <ntzeitlin@gmail.com>
2025-03-21 11:13:55 -0500

    removed unnecessary code

--- raterapi/views/categories.py
+++ raterapi/views/categories.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework import serializers
 from rest_framework.response import Response
 from raterapi.models import Category
