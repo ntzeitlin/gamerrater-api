adfbd46b344cb51247bdef0d30395773b0fcaa1c
Nate Z <ntzeitlin@gmail.com>
2025-03-21 14:30:35 -0500

    commented out unused code

--- raterapi/views/__init__.py
+++ raterapi/views/__init__.py
@@ -1,5 +1,6 @@
 from .auth import login_user, register_user
-from .users import UserViewSet
+
+# from .users import UserViewSet
 from .game import GameViewSet
 from .categories import CategoryViewSet
 from .review import ReviewViewSet
--- raterapi/views/users.py
+++ raterapi/views/users.py
@@ -15,35 +15,35 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
 
-class UserViewSet(viewsets.ViewSet):
-    queryset = User.objects.all()
-    permission_classes = [permissions.AllowAny]
+# class UserViewSet(viewsets.ViewSet):
+#     queryset = User.objects.all()
+#     permission_classes = [permissions.AllowAny]
 
-    @action(detail=False, methods=["post"], url_path="register")
-    def register_account(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = User.objects.create_user(
-                username=serializer.validated_data["username"],
-                first_name=serializer.validated_data["first_name"],
-                last_name=serializer.validated_data["last_name"],
-                password=serializer.validated_data["password"],
-            )
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     @action(detail=False, methods=["post"], url_path="register")
+#     def register_account(self, request):
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             user = User.objects.create_user(
+#                 username=serializer.validated_data["username"],
+#                 first_name=serializer.validated_data["first_name"],
+#                 last_name=serializer.validated_data["last_name"],
+#                 password=serializer.validated_data["password"],
+#             )
+#             token, created = Token.objects.get_or_create(user=user)
+#             return Response({"token": token.key}, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=False, methods=["post"], url_path="login")
-    def user_login(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
+#     @action(detail=False, methods=["post"], url_path="login")
+#     def user_login(self, request):
+#         username = request.data.get("username")
+#         password = request.data.get("password")
 
-        user = authenticate(username=username, password=password)
+#         user = authenticate(username=username, password=password)
 
-        if user:
-            token = Token.objects.get(user=user)
-            return Response({"token": token.key}, status=status.HTTP_200_OK)
-        else:
-            return Response(
-                {"error": "Invalid Credentials"}, status=status.HTTP_400_BAD_REQUEST
-            )
+#         if user:
+#             token = Token.objects.get(user=user)
+#             return Response({"token": token.key}, status=status.HTTP_200_OK)
+#         else:
+#             return Response(
+#                 {"error": "Invalid Credentials"}, status=status.HTTP_400_BAD_REQUEST
+#             )
